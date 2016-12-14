cc7d788c9a7a0f607872d99cc3f11113184f02bd
Dadidam <joker.pnz@gmail.com>
2016-12-14 13:09:20 +0300

    No comments *lol*

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
+import Editor from './Editor';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+	  super();
+	  
+	  this.state = {
+		  showEditor: false
+	  };
+	  
+	  this.showEditorHandle = this.showEditorHandle.bind(this);
+  }
+  showEditorHandle() {
+	  this.setState({
+		  showEditor: !this.state.showEditor
+	  });
+  }
   render() {
+	const btnTitle = this.state.showEditor ? 'Show editor' : 'Hide editor';
+	
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Employee List App</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-content">
+		  <button onClick={this.showEditorHandle} title={btnTitle} />
+          <div className="App-list">List</div>
+          <div className="App-editor"><Editor /></div>
+		</div>
       </div>
     );
   }
